e09e084a89ee557e00f1037b0493c7b042cd52cc
alecthw <alecthw@163.com>
2024-07-16 15:58:40 +0800

    svelte 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn">Hello daisyUI</button>
+<script>
+  let count = $state(0);
+  let double = $derived(count * 2);
+
+  $effect(() => {
+    if (count > 10) {
+      alert('Too high!');
+    }
+  });
+</script>
+
+<div class="flex flex-col items-center justify-center">
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  <button class="btn" onclick={() => count++}>
+    {count} / {double}
+  </button>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,11 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // opt in to runes mode
+  compilerOptions: {
+    runes: true,
+  },
+
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
