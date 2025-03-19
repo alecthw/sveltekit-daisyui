b16bd2564e6e148707bccb80847619e78339aac0
alecthw <alecthw@163.com>
2025-03-19 22:17:56 +0800

    1. update dependencies 2. add vite compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { compression } from 'vite-plugin-compression2';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    compression({
+      algorithm: 'gzip',
+    }),
+    // compression({
+    //   algorithm: 'brotliCompress',
+    // }),
+  ],
 });
